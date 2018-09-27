d372f73b8bc509738c5cf274fd094d01f8bfa799
HamSeungPyo <39762906+HamSeungPyo@users.noreply.github.com>
2018-09-27 13:57:59 +0900

    Move 스크립트 수정
    기타 등등 메모 추가

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	private int a;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//기타 등등
+	
+}
